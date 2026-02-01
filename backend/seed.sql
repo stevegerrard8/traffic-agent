6dd492f53e81c52ed9ac0598c658fe39b0f51498
Serhat UYAR <109991190+stevegerrard8@users.noreply.github.com>
2026-02-02 00:29:12 +0300

    Create seed.sql

---
+++ backend/seed.sql
@@ -0,0 +1,9 @@
+INSERT OR IGNORE INTO users (email, password, plan)
+VALUES ('demo@user.com', '1234', 'pro');
+
+INSERT INTO accidents (date, city, type, result) VALUES
+('2026-01-10','İstanbul','Çarpışma','Ölümlü'),
+('2026-01-11','İstanbul','Yaya','Yaralı'),
+('2026-01-12','Ankara','Tek Araç','Yaralı'),
+('2026-01-13','İzmir','Çarpışma','Maddi Hasar'),
+('2026-01-14','İstanbul','Çarpışma','Yaralı');
